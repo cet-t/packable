49eda5be773126cbf1ceeea63395480866db0fa9
cet-t <cets021@gmail.com>
2023-02-03 01:18:31 +0900

    仮止め

--- packable/main.py
+++ packable/main.py
@@ -1,24 +1,19 @@
-import asyncio
-import typing
-from typing import Any
-import discord
+﻿import discord
 from discord.ext import commands
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='>', intents=intents)
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 
 @bot.command()
-async def ping(m):
-    await m.send('pong')
-
-
-async def help(m) -> None:
-    await m.send('pong')
+async def test(ctx):
+    await ctx.send("test sent")
 
 
 if __name__ == "__main__":
-    from os import *
+    import os
     import pathlib
 
     import discord
@@ -26,35 +21,15 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    file = pathlib.Path(__file__).resolve()
-    prefix = file.parent
+    file = pathlib.Path(__file__)
 
     try:
-        token = environ["TOKEN"]
+        token = os.environ["TOKEN"]
     except KeyError:
-        token = environ["ERROR"]
+        token = os.environ["ERROR"]
 
-    intents = discord.Intents.all()
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user} is ready")
 
-    class MyBot(commands.Bot):
-        async def on_ready(self):
-            print("Ready")
-
-        async def setup_hook(self):
-            await self.load_extension(file.stem)
-
-            try:
-                guild_id = getenv["GUILD"]
-            except KeyError:
-                guild_id = environ["ERROR"]
-
-            guild = self.get_guild(guild_id)
-            if guild is None:
-                guild = await self.fetch_guild(guild_id)
-
-            self.tree.copy_global_to(guild=guild)
-            await self.tree.sync(guild=guild)
-            await self.tree.sync()
-
-    bot = MyBot("t!", intents=intents)
     bot.run(token)
