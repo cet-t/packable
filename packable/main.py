7a7cc5e08e1f4271ef9bcb5f35358c48b5ccbf95
cet-t <cets021@gmail.com>
2023-02-02 23:49:00 +0900

    naniga dameka wakaran

--- packable/main.py
+++ packable/main.py
@@ -1,15 +1,51 @@
-import discord
-from discord.ext import commands as cmd
+import asyncio
+import typing
+from typing import Any
 
-import os
+import discord
+from discord.ext import commands
+
 
 intents = discord.Intents.all()
-bot = cmd.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix='>', intents=intents)
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-token = os.environ("TOKEN")
-bot.run(token)
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    import discord
+    from dotenv import load_dotenv as denv
+
+    denv()
+
+    file = Path(__file__).resolve()
+    prefix = file.parent
+    # try:
+    #     token = os.environ["token"]
+    # except KeyError:
+    #     token = os.environ["DIS_TEST_TOKEN"]
+
+    intents = discord.Intents.all()
+
+    class MyBot(commands.Bot):
+        async def on_ready(self) -> None:
+            print("ready")
+
+        async def setup_hook(self) -> None:
+            await self.load_extension(file.stem)
+            guild_id = ID_HERE
+            guild = self.get_guild(guild_id)
+            if guild is None:
+                guild = await self.fetch_guild(guild_id)
+
+            self.tree.copy_global_to(guild=guild)
+            await self.tree.sync(guild=guild)
+            await self.tree.sync()
+
+    bot = MyBot("t!", intents=intents)
+    bot.run(token)
